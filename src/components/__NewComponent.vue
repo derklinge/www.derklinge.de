34eb37092f8e31af8f59beb8527090c0ef584920
derklinge <github@derklinge.de>
2019-03-21 21:29:33 +0100

    Add scaffolding template file for new Vue components

---
+++ src/components/__NewComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    class=""
+  >
+    ...
+  </div>
+</template>
+
+<script>
+// import NestedComponent from '@/components/NestedComponent
+
+export default {
+  name: 'NewComponent',
+
+  components: {
+    // NestedComponent,
+  },
+
+  data() {
+    return {
+      // ...
+    }
+  },
+
+  methods: {
+    // ...
+  },
+}
+</script>
+
+<style lang="scss">
+/*
+.new-component {
+}
+*/
+</style>
